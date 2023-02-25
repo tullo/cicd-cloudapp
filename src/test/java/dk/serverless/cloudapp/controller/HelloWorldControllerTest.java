77628a3eda8a558d12858b6cebf64063075942b6
Andreas Amstutz <tullo@users.noreply.github.com>
2023-02-25 13:26:00 +0100

    fix import for test related class

--- src/test/java/dk/serverless/cloudapp/controller/HelloWorldControllerTest.java
+++ src/test/java/dk/serverless/cloudapp/controller/HelloWorldControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
